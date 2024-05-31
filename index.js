3550eb1738cd6e809847827c667b7f881fb97903
ChisomSylvia <chisomsylvia95@gmail.com>
2024-05-31 01:52:28 +0100

    Modified the hamburger icon behaviour

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ window.onscroll = function() {
     nav.style.backdropFilter = 'none';
   }
 }
+
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+})
+
+document.querySelectorAll('.nav-item').forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
